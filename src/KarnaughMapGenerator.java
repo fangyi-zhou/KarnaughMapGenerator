2ed7b5d674f7ff30dcf6c4c4ac4b3bd13df0d62c
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2017-01-17 13:18:06 +0000

    Input modifications

--- src/KarnaughMapGenerator.java
+++ src/KarnaughMapGenerator.java
@@ -4,9 +4,14 @@ public class KarnaughMapGenerator {
     private static StateTable sTable;
 
     private static void readData(){
-        System.out.println("Please enter the number of states:");
+        System.out.println("Please enter the number of states: (1 to 8 inclusive)");
         int noOfStates = IOUtil.readInt();
+        while (noOfStates > 8 || noOfStates < 1) {
+            System.out.println("Invalid input, try again");
+            noOfStates = IOUtil.readInt();
+        }
         sTable = new StateTable(noOfStates);
+        System.out.println("An alias is a name to identify a state.");
         for (int i = 0; i < noOfStates; i++) {
             System.out.println("Please enter the alias for State " + i + ":" );
             sTable.setAlias(i, IOUtil.readString());
@@ -15,20 +20,24 @@ public class KarnaughMapGenerator {
         int[] transitionsTrue = new int[noOfStates];
         int[] transitionsFalse = new int[noOfStates];
         for (int i = 0; i < noOfStates; i++) {
-            System.out.println("Please enter the destination state for "+ sTable.getAlias(i) + " for a true input:");
+            System.out.println("Please enter the destination state (alias) for "+ sTable.getAlias(i) + " for a true input:");
             transitionsTrue[i] = sTable.getState(IOUtil.readString());
             while (transitionsTrue[i] < 0) {
                 System.out.println("Invalid input, try again");
                 transitionsTrue[i] = sTable.getState(IOUtil.readString());
             }
-            System.out.println("Please enter the destination state for "+ sTable.getAlias(i) + " for a false input:");
+            System.out.println("Please enter the destination state (alias) for "+ sTable.getAlias(i) + " for a false input:");
             transitionsFalse[i] = sTable.getState(IOUtil.readString());
             while (transitionsFalse[i] < 0) {
                 System.out.println("Invalid input, try again");
                 transitionsFalse[i] = sTable.getState(IOUtil.readString());
             }
-            System.out.println("Please enter the state representation for "+ sTable.getAlias(i) + ":");
+            System.out.println("Please enter the state representation (number) for "+ sTable.getAlias(i) + ":");
             stateRep[i] = IOUtil.readInt();
+            if (stateRep[i] < 0 || stateRep[i] >= noOfStates) {
+                System.out.println("Invalid input, try again");
+                stateRep[i] = IOUtil.readInt();
+            }
         }
         tTable = new TransitionTable(noOfStates, transitionsFalse, transitionsTrue, sTable);
         tTable.updateStateRep(stateRep);
@@ -38,7 +47,7 @@ public class KarnaughMapGenerator {
         int noOfStates = sTable.getNoOfStates();
         int[] stateRep = new int[noOfStates];
         for (int i = 0; i < noOfStates; i++) {
-            System.out.println("Please enter the state representation for "+ sTable.getAlias(i) + ":");
+            System.out.println("Please enter the state representation (number) for "+ sTable.getAlias(i) + ":");
             stateRep[i] = IOUtil.readInt();
         }
         tTable.updateStateRep(stateRep);
